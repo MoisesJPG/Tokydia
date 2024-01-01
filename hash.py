fb8f1e05ae3124fb7f6e912a5873680d36eb93ce
MoisesJPG <mojupega1610@gmail.com>
2024-01-01 18:22:22 +0100

    Update

--- hash.py
+++ hash.py
@@ -1,6 +1,17 @@
 import hashlib
 
 while True:
-    animeName = input("Nombre del anime: ")
-    animeId = hashlib.sha256(animeName.encode('utf-8')).hexdigest()[:16]
-    print(f"Id: {animeId}\n")
+    # Solicita al usuario ingresar un nombre de anime
+    anime_name = input("Ingresa el nombre del anime (o 'exit' para salir): ")
+
+    # Verifica si el usuario desea salir
+    if anime_name.lower() == 'exit':
+        break
+
+    # Calcula el hash SHA-256 y muestra los primeros 16 caracteres
+    hash_value = hashlib.sha256(anime_name.encode('utf-8')).hexdigest()[:16]
+    print(f"Hash SHA-256 del nombre '{anime_name}': {hash_value}")
+    print()
+
+# Mensaje de despedida
+print("¡Adiós!")
